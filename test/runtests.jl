d47f5c8e744694619dc7067e47532178609568fb
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-15 01:33:51 +0000

    Snapshot of branch master taken on 2019-04-14T21:33:49.663-04:00 from "https://github.com/UnofficialJuliaMirror/ParallelProcessingTools.jl-8e8a01fc-6193-5ca1-a2f1-20776dae4199"

---
+++ test/runtests.jl
@@ -0,0 +1,10 @@
+# This file is a part of ParallelProcessingTools.jl, licensed under the MIT License (MIT).
+
+import Test
+Test.@testset "Package ParallelProcessingTools" begin
+    include("test_threadsafe.jl")
+    include("test_threadlocal.jl")
+    include("test_workpartition.jl")
+    include("test_onthreads.jl")
+    include("test_onprocs.jl")
+end
